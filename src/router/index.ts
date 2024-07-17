c64284ba02af72f9fd7133400d4a16c21b1738f2
Cupid Valentine <westerngod@126.com>
2024-07-17 23:16:40 +0800

    refactor: optimized codes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
-import gsap from 'gsap'
+
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import { animatePageOut } from '@/utils/animation'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,43 +22,7 @@ const router = createRouter({
   ]
 })
 
-const animatePageIn = () => {
-  const bannerOne = document.getElementById('banner-1')
-  const bannerTwo = document.getElementById('banner-2')
-  const bannerThree = document.getElementById('banner-3')
-  const bannerFour = document.getElementById('banner-4')
 
-  if (bannerOne && bannerTwo && bannerThree && bannerFour) {
-    const tl = gsap.timeline()
-
-    tl.set([bannerOne, bannerTwo, bannerThree, bannerFour], {
-      yPercent: 0
-    }).to([bannerOne, bannerTwo, bannerThree, bannerFour], {
-      yPercent: 100,
-      stagger: 0.2,
-    })
-  }
-}
-
-const animatePageOut = (next) => {
-  const bannerOne = document.getElementById('banner-1')
-  const bannerTwo = document.getElementById('banner-2')
-  const bannerThree = document.getElementById('banner-3')
-  const bannerFour = document.getElementById('banner-4')
-  if (bannerOne && bannerTwo && bannerThree && bannerFour) {
-    const tl = gsap.timeline()
-    tl.set([bannerOne, bannerTwo, bannerThree, bannerFour], {
-      yPercent: -100
-    }).to([bannerOne, bannerTwo, bannerThree, bannerFour], {
-      yPercent: 0,
-      stagger: 0.2,
-      onComplete: () => {
-        next()
-        animatePageIn()
-      }
-    })
-  }
-}
 
 router.beforeEach((to, from, next) => {
   animatePageOut(next)
---
+++ src/utils/animation.ts
@@ -0,0 +1,40 @@
+import type { NavigationGuardNext } from "vue-router"
+import gsap from 'gsap'
+
+export const animatePageIn = () => {
+  const bannerOne = document.getElementById('banner-1')
+  const bannerTwo = document.getElementById('banner-2')
+  const bannerThree = document.getElementById('banner-3')
+  const bannerFour = document.getElementById('banner-4')
+
+  if (bannerOne && bannerTwo && bannerThree && bannerFour) {
+    const tl = gsap.timeline()
+
+    tl.set([bannerOne, bannerTwo, bannerThree, bannerFour], {
+      yPercent: 0
+    }).to([bannerOne, bannerTwo, bannerThree, bannerFour], {
+      yPercent: 100,
+      stagger: 0.2,
+    })
+  }
+}
+
+export const animatePageOut = (next: NavigationGuardNext) => {
+  const bannerOne = document.getElementById('banner-1')
+  const bannerTwo = document.getElementById('banner-2')
+  const bannerThree = document.getElementById('banner-3')
+  const bannerFour = document.getElementById('banner-4')
+  if (bannerOne && bannerTwo && bannerThree && bannerFour) {
+    const tl = gsap.timeline()
+    tl.set([bannerOne, bannerTwo, bannerThree, bannerFour], {
+      yPercent: -100
+    }).to([bannerOne, bannerTwo, bannerThree, bannerFour], {
+      yPercent: 0,
+      stagger: 0.2,
+      onComplete: () => {
+        next()
+        animatePageIn()
+      }
+    })
+  }
+}
